5791abb285df122a36e62c0bc6f8e5a87a325f4c
Steve Calla <callasteven@gmail.com>
2022-08-28 18:55:17 -0600

    Add delete function

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const notes = express.Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
+const fs = require('fs');
+const { parse } = require('path');
 
 notes.route('/')
   // GET Route for retrieving all the notes
@@ -25,5 +27,38 @@ notes.route('/')
       res.error('Error in adding note');
     }
   })
+
+  notes.route('/:id')
+    .delete((req, res) => {
+      console.log('hello', req.params.id);
+      res.send("Delete request");
+
+      fs.readFile('./db/notes.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(JSON.parse(data));
+          const parsedData = JSON.parse(data);
+          console.log(parsedData.length)
+          
+          for (let i = 0; i < parsedData.length; i++) {
+            if(parsedData[i].note_id === req.params.id) {
+              console.log('MATCH =', i);
+              parsedData.splice(i, 1);
+            } else {
+              console.log('NO MATCH ', i);
+            }
+          }
+
+          console.log(parsedData);
+          console.log(JSON.stringify(parsedData))
+
+          fs.writeFile('./db/notes.json', JSON.stringify(parsedData, null, 4), (err) =>
+          err ? console.error(err) : console.info(`\nData written to ${destination}`)
+          );
+        }
+ 
+      });
+    })
 
 module.exports = notes;
